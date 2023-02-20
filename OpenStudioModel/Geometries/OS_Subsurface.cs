76d858cbf51bd6fef4c8f7f78e40f0ad86272fe3
Kunyu Luo <kunyuluo@usc.edu>
2023-02-20 18:18:08 +0800

    Modify project directories

---
+++ OpenStudioModel/Geometries/OS_Subsurface.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenStudio;
+
+namespace OpenStudioModel.Geometries
+{
+    class OS_Subsurface
+    {
+        Surface srf = null;
+
+        public OS_Subsurface(
+            Model model,
+            List<List<double>> vertices,
+            string surfaceType = "FixedWindow",
+            ConstructionBase constructionBase = null,
+            Space space = null)
+        {
+            srf = null;
+        }
+    }
+}
