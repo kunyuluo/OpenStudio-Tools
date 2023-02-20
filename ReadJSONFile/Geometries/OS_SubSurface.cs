a9038382fb46a6c64398dfffcdd0b3c4b3988ab4
Kunyu Luo <kunyuluo@usc.edu>
2023-02-20 17:41:11 +0800

    Add program file

---
+++ ReadJSONFile/Geometries/OS_SubSurface.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenStudio;
+using ReadJSONFile.Geometries;
+
+namespace ReadJSONFile.Geometries
+{
+    public class OS_SubSurface
+    {
+        Surface srf = null;
+
+        public OS_SubSurface(
+            Model model,
+            List<List<double>> vertices,
+            string surfaceType = "FixedWindow",
+            ConstructionBase constructionBase = null,
+            Space space = null)
+        {
+            srf = null;
+        }
+    }
+}
